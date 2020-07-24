839e7c0c1f8ba498c6e9feaddf25dd18da047863
Allison Sadin <aesadin@gmail.com>
2020-07-24 11:06:10 -0700

    failed first vendor test

--- Bakery.Tests/ModelTests/VendorTests.cs
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -5,5 +5,18 @@ using System;
 
 namespace Bakery.Tests
 {
-
+    [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Order newVendor = new Vendor("test", "test2", "Test4");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
 }
