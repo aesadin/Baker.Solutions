e7a77c52ccf37b7900f5d654ac21266ecdccaa7b
Allison Sadin <aesadin@gmail.com>
2020-07-24 14:32:48 -0700

    passed tests for order constructor properties

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -26,5 +26,21 @@ namespace Bakery.Tests
       List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrder_OrderList()
+    {
+      string title1 = "title1";
+      string title2 = "title2";
+      string description1 = "this is title1";
+      string description2 = "this is title 2";
+      string date1 = "this date";
+      string date2 = "that date";
+      Order newItem1 = new Order(title1, description1, date1);
+      Order newItem2 = new Order(title2, description2, date2);
+      List<Order> newList = new List<Order> { newItem1, newItem2 };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
