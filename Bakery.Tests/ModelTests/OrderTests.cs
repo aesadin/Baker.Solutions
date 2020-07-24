8e4168491830000a66087aa679ee786e1320d872
Allison Sadin <aesadin@gmail.com>
2020-07-24 09:51:05 -0700

    constructor test failed

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -5,5 +5,18 @@ using System;
 
 namespace Bakery.Tests
 {
-  
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
 }
